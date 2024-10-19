98c3bc9a4877892eff33495535c21fd7e9d9e5ca
Kevin Heredia <kevin@ravecodesolutions.com>
2024-10-19 12:49:43 -0500

    git init

--- iOS-Avanzado-Dbz/Presentation/Splash/SplashViewController.swift
+++ iOS-Avanzado-Dbz/Presentation/Splash/SplashViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashViewController.swift
 //  iOS-Avanzado-Dbz
 //
 //  Created by Kevin Heredia on 19/10/24.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+     
     }
 
 
-}
 
+}
