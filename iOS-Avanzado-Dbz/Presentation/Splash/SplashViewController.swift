7d8a1dbe04cc5b2ccba3839b30ebb6dfe2e9df69
Kevin Heredia <kevin@ravecodesolutions.com>
2024-10-19 12:53:36 -0500

    Splash Controller added

--- iOS-Avanzado-Dbz/Presentation/Splash/SplashViewController.swift
+++ iOS-Avanzado-Dbz/Presentation/Splash/SplashViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-
+    
+    init(){
+        super.init(nibName: String(describing: SplashViewController.self), bundle: Bundle(for: type(of: self)))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
